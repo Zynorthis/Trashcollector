607436c7598ad1fff4240d352868bee2021f4dee
Jacob Taylor <jacobtaylor727@outlook.com>
2019-01-11 09:29:02 -0600

    started work on implimenting roles into trashcollecter. On startup, the program will now check for certain roles and if they do not exist it will add them to the database. Hard coded one admin user to always be in the system.

---
+++ TrashCollector/Models/Days.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Days
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display (Name = "Day")]
+        public string Day { get; set; }
+        public bool IsWeekDay { get; set; }
+    }
+}
--- TrashCollector/Startup.cs
+++ TrashCollector/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TrashCollector.Models;
+using Microsoft.AspNet.Identity;
 
 [assembly: OwinStartupAttribute(typeof(TrashCollector.Startup))]
 namespace TrashCollector
@@ -9,6 +12,43 @@ namespace TrashCollector
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "Bassic";
+                user.Email = "admin@test.com";
+
+                string userPassword = "H@rdC0d3d";
+
+                var chkUser = userManager.Create(user, userPassword);
+
+                if (chkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
         }
     }
 }
